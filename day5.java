e636dbf494207f902495ee78754819710a161f4d
Subhakar R <rsubhakar222@gmail.com>
2025-01-25 12:35:31 +0530

    Add files via upload

---
+++ day5.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class day5 {
+    public static void main(String args[]) {
+        Date current_date = new Date();
+
+        System.out.println(current_date);
+    }
+}
